ea16992be1e4bad90b97aead6d879d72ed497c46
sanek a-ver <sasacornij103@gmail.com>
2026-05-21 09:48:07 +0300

    AVERBOT

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Query = require("samp-query");
+
+const SERVER_IP = "51.91.91.92";
+const SERVER_PORT = 7777;
+
+console.log("AVERBOT STARTING...");
+
+/* CHECK SERVER */
+
+setInterval(() => {
+
+    Query(
+        {
+            host: SERVER_IP,
+            port: SERVER_PORT
+        },
+        (err, res) => {
+
+            if(err)
+            {
+                console.log("SERVER OFFLINE");
+                return;
+            }
+
+            console.log(
+    "ONLINE:",
+    res.online,
+    "/",
+    res.maxplayers
+);
+
+        }
+    );
+
+},5000);
